fd1d931c39bbc75de971b69b28ffbc41f62efcf1
Michael <msjia@me.com>
2021-02-21 01:55:48 +0800

    Implement Brewing Service to trigger Brewing - close #28

--- src/main/java/my/selfstudy/msscbeerservice/repository/BeerRepository.java
+++ src/main/java/my/selfstudy/msscbeerservice/repository/BeerRepository.java
@@ -1,16 +1,15 @@
 package my.selfstudy.msscbeerservice.repository;
 
 import my.selfstudy.msscbeerservice.domain.Beer;
-import my.selfstudy.msscbeerservice.web.model.BeerDto;
 import my.selfstudy.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
 
     Page<Beer> findAllByBeerName(String beerName, PageRequest pageRequest);
