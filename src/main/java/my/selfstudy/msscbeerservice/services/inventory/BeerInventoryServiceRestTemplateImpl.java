5ceb3a686caae03a6329fc3b8253704408444459
Michael <msjia@me.com>
2021-04-05 01:20:23 +0800

    add discovery

--- src/main/java/my/selfstudy/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/my/selfstudy/msscbeerservice/services/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -5,6 +5,7 @@ import my.selfstudy.msscbeerservice.services.inventory.model.BeerInventoryDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-//@Profile("!local-discovery")
+@Profile("!local-discovery")
 @Slf4j
 @ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = true)
 @Component
